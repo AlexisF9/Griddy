27a45e082e0a8b31c87d4aa48df659a7776557a1
AlexisF9 <alexis.flacher38@gmail.com>
2024-12-05 15:40:55 +0100

    add tasks + add task and remove task functions

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,43 +1,111 @@
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 
 function Tasks() {
-  const model = [
-    {
-      name: "Colonne 1",
-      cards: [
-        {
-          label: "Ma première card",
-        },
-      ],
-    },
-    {
-      name: "Colonne 2",
-      cards: [
-        {
-          label: "Ma deuxième card",
-        },
-      ],
-    },
-  ];
+  const [newColumn, setNewColumn] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasksList();
+  }, []);
+
+  const getTasksList = () => {
+    setTasks(
+      localStorage.getItem("tasks")
+        ? JSON.parse(localStorage.getItem("tasks") ?? "")
+        : []
+    );
+  };
+
+  const createColumn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+
+    const oldTasks = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks") ?? "")
+      : [];
+
+    if (
+      !oldTasks.find((el: { name: string }) => el.name === data.get("name"))
+    ) {
+      const createCol = {
+        name: data.get("name"),
+        id: Date.now(),
+        cards: [],
+      };
+
+      const newTasks = [...oldTasks, createCol];
+      localStorage.setItem("tasks", JSON.stringify(newTasks));
+      setNewColumn(false);
+      getTasksList();
+    } else {
+      window.alert("Cette colonne existe déjà");
+    }
+  };
+
+  const removeColumn = (id: number) => {
+    const col = tasks.find((el: { id: number }) => el.id === id);
+    if (col) {
+      const tasksCopy = [...tasks];
+      const index = tasksCopy.indexOf(col);
+      tasksCopy.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasksCopy));
+      getTasksList();
+    }
+  };
 
   return (
     <div className="c-tasks">
       <div className="c-tasks__intro">
-        <h2 className="c-h-l u-mb-8">Mes taches</h2>
-        <Button color="secondary" label="Ajouter une colonne " />
+        <h2 className="c-h-l u-mb-16">Mes taches</h2>
+        {newColumn ? (
+          <div className="c-tasks__new-col">
+            <form onSubmit={(e) => createColumn(e)}>
+              <label htmlFor="name">Nom de la colonne</label>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                className="c-input"
+                required
+              />
+              <div>
+                <Button type="submit" color="secondary" label="Créer" />
+                <Button label="Annuler" onClick={() => setNewColumn(false)} />
+              </div>
+            </form>
+          </div>
+        ) : (
+          <Button
+            color="secondary"
+            label="Ajouter une colonne"
+            onClick={() => setNewColumn(true)}
+          />
+        )}
       </div>
 
       <div className="c-table">
-        {model.map((el) => (
-          <div className="c-table__col">
-            <p className="c-text-l u-mb-12">{el.name}</p>
-            <div>
-              {el.cards.map((card) => (
-                <div className="c-table__card">{card.label}</div>
-              ))}
+        {tasks && tasks.length > 0 ? (
+          tasks.map((el: any) => (
+            <div className="c-table__col" key={el.id}>
+              <p className="c-text-l u-mb-12">{el.name}</p>
+              <Button onClick={() => removeColumn(el.id)} label="Supprimer" />
+              <div>
+                {el.cards.length > 0 &&
+                  el.cards.map((card: any) => (
+                    <div className="c-table__card" key={card.id}>
+                      {card.label}
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>
+            Vous n'avez aucune tache. Crée une colonne pour ajouter une tâche.
+          </p>
+        )}
       </div>
     </div>
   );
