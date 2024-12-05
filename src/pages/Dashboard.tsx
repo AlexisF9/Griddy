db188610bb208c72bf04e0c70ecf71cc9c07fd58
AlexisF9 <alexis.flacher38@gmail.com>
2024-12-05 15:51:26 +0100

    add vercel json file

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react";
 import { useAppStore } from "../App";
 
 function Dashboard() {
+  const [tasks, setTasks] = useState([]);
+
   const { name } = useAppStore();
   const user = name ?? "";
 
+  useEffect(() => {
+    setTasks(
+      localStorage.getItem("tasks")
+        ? JSON.parse(localStorage.getItem("tasks") ?? "")
+        : []
+    );
+  }, []);
+
   return (
     <div className="c-dashboard">
-      <h2 className="c-dashboard__title">
+      <h2 className="c-dashboard__title c-h-xl u-mb-8">
         Bienvenue sur ton board <span>{user}</span>
       </h2>
-      <h3>Statistiques</h3>
+      <h3>Statistiques :</h3>
+      <p>Nombre de colonnes : {tasks.length}</p>
     </div>
   );
 }
