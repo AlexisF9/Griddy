caba63a77a2074be62c9aabe38d1af29da2f7c5f
AlexisF9 <alexis.flacher38@gmail.com>
2025-04-02 15:04:42 +0200

    add react pdf lib + create pdf modal component + add pdf view button in task infos

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./assets/styles/style.scss";
 import App from "./App.tsx";
+import { pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 createRoot(document.getElementById("root")!).render(<App />);
